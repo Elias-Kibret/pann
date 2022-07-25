28bd64ef16e1f71368e386d954d73ead3bab0e52
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-07-25 08:19:14 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import Welcome from "./Components/Welcome";
 function App() {
   return (
-    <div className="App">
+    <div className="App overflow-hidden w-[414px] mx-auto  bg-[#00918f] ">
       <Welcome/>
     </div>
   );
